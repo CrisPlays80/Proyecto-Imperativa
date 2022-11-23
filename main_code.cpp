d0b713d51c98d5aba7fa4da20eec88f9685e7695
CrisPlays80 <79766260+CrisPlays80@users.noreply.github.com>
2022-11-23 14:42:23 -0500

    Este es el codigo main

---
+++ main_code.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+
+int menu(string name){
+    int opc_menu;
+    cout << "\t\tMenu de " << name << endl;
+    cout << "1. Temporizador\n"
+    "2. Clases";
+    cin >> opc_menu;
+    return opc_menu;
+}
+
+string login(){
+    string name, lastName;
+    cout << "Nombre: " << endl; getline(cin, name);
+    cout << "Apellido: " << endl; getline(cin, lastName);
+    return name + " " + lastName;
+}
+
+int main(){
+    string log;
+    int menu_continue;
+    cout << "\t\tBookNote\n\tSe eficaz en tu estudio" << endl;
+    cout << "Ingresar Sesion:" <<endl;
+    log = login();
+    system("cls");
+
+    do{
+        menu_continue = menu(log);
+    }while(menu_continue != 0);
+
+    return 0;
+}
